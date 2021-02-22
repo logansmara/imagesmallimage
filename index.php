660378097bc2c5e51018ab58d700989ba2176a7f
Anver Latypov <a120671@mail.ru>
2021-02-22 17:12:32 +0400

    first

---
+++ index.php
@@ -0,0 +1,71 @@
+<style>
+@media (min-width: 768px) {
+  img {
+  	max-width: 100% !important;
+  }
+  .bigimg {
+    display: block;
+  }
+  .smallimg {
+    display: none;
+  }
+}
+@media (max-width: 767px) {
+  .bigimg {
+  	display: none;
+  }
+  .smallimg {
+  	display: block;
+  }
+}
+</style>
+<?php
+$text = '<p><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">В мастерской я работаю одна. Что это значит? Больше своих возможностей сделать не успею.⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">Посчитала реальное количество наборов ёлочных украшений. Получилось 15 со снежинками и 10 из звёздочек.⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">А часть, причём приличную, уже забронировали! Сколько осталось?⁣⁣⠀</span><br />
+<img alt="❄" class="emoji" src="https://vk.com/emoji/e/e29d84.png" style="border:0px; color:transparent; display:inline-block; font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px; height:16px; margin:0px 1px; overflow:hidden; vertical-align:-3px; width:16px" /><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">&nbsp;Снежинки ⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">6шт с разным рисунком⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">размер 8см⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">Набор 2100₽⁣⁣⠀</span><br />
+<img alt="❗" class="emoji" src="https://vk.com/emoji/e/e29d97.png" style="border:0px; color:transparent; display:inline-block; font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px; height:16px; margin:0px 1px; overflow:hidden; vertical-align:-3px; width:16px" /><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">свободно 7 наборов⁣⁣⠀</span><br />
+<img alt="✨" class="emoji" src="https://vk.com/emoji/e/e29ca8.png" style="border:0px; color:transparent; display:inline-block; font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px; height:16px; margin:0px 1px; overflow:hidden; vertical-align:-3px; width:16px" /><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">Звёздочки⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">3 шт из матового стекла⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">чёрный или светлый металл⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">размер 16см⁣⁣⠀</span><br />
+<span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">Набор 1800₽⁣⁣⠀</span><br />
+<img alt="❗" class="emoji" src="https://vk.com/emoji/e/e29d97.png" style="border:0px; color:transparent; display:inline-block; font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px; height:16px; margin:0px 1px; overflow:hidden; vertical-align:-3px; width:16px" /><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px">свободно 6 наборов⁣⁣⠀</span></p>
+
+<p><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px"><img alt="" src="/images/authors/ksyusha-delaet/ks-6-1.jpg" style="height:500px; width:800px" /></span></p>
+
+<p><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px"><img alt="" src="/images/authors/ksyusha-delaet/ks-6-2.jpg" style="height:500px; width:800px" /></span></p>
+
+<p><span style="color:rgb(0, 0, 0); font-family:-apple-system,blinkmacsystemfont,roboto,helvetica neue,geneva,noto sans armenian,noto sans bengali,noto sans cherokee,noto sans devanagari,noto sans ethiopic,noto sans georgian,noto sans hebrew,noto sans kannada,noto sans khmer,noto sans lao,noto sans osmanya,noto sans tamil,noto sans telugu,noto sans thai,sans-serif,arial,tahoma,verdana; font-size:15px"><img alt="" src="/images/authors/ksyusha-delaet/ks-6-3.jpg" style="height:500px; width:800px" /></span></p>
+
+<p>&nbsp;</p>
+';
+
+$text_array = explode('<img alt="" src="/images/authors/ksyusha-delaet/', $text);
+unset($text_array[0]);
+//print_r($text_array);
+$img = '';
+foreach ($text_array as $value) {
+	$img = $img . '<img alt="" src="/images/authors/ksyusha-delaet/' . $value . '+++';
+}
+$img = substr($img, 0, -3);
+$img_array = explode('+++', $img);
+$img_string = '';
+foreach ($img_array as $value) {
+	$img_string = $img_string . stristr($value, '>', true) . '>+++';
+}
+$img_string = substr($img_string, 0, -3);
+$image_aray = explode('+++', $img_string);
+foreach ($image_aray as $value) {
+	$smallvalue = str_replace('authors/', 'authors/small', $value);
+	$smallvalueclass = str_replace('src="/images/authors', 'class="smallimg" src="/images/authors', $smallvalue);
+	$smallvalueclass = str_replace('style="height:500px; width:800px"', '', $smallvalueclass);
+	$valueclass = str_replace('src="/images/authors', 'class="bigimg" src="/images/authors', $value);
+	$valueclass = str_replace('style="height:500px; width:800px"', '', $valueclass);
+	$smallvaluevalueclass = $valueclass . $smallvalueclass;
+	$text = str_replace($value, $smallvaluevalueclass, $text);
+}
+echo $text;
